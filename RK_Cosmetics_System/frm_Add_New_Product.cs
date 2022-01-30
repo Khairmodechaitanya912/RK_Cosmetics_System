58a877e53b6b809d11db14aad898c10b57b7658a
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-01-30 21:08:44 +0530

    Some Change

--- RK_Cosmetics_System/frm_Add_New_Product.cs
+++ RK_Cosmetics_System/frm_Add_New_Product.cs
@@ -53,7 +53,7 @@ namespace RK_Cosmetics_System
             {
                 Cmd.Connection = Con;
 
-                Cmd.CommandText = "Select Max(Employee_ID) from Product_Details";
+                Cmd.CommandText = "Select Max(Product_ID) from Product_Details";
 
                 Cnt = Convert.ToInt32(Cmd.ExecuteScalar()) + 1;
             }
@@ -139,7 +139,40 @@ namespace RK_Cosmetics_System
 
         private void btn_Save_Click(object sender, EventArgs e)
         {
+            Con_Open();
 
+            if (tb_Product_ID.Text != "" && cmb_Brand_Name.Text != "" && tb_Product_Name.Text != "" && tb_Purchase_Price.Text != "" && tb_Selling_Price.Text != "" && tb_Stock.Text != "" && tb_GST.Text != "")
+            {
+                SqlCommand Cmd = new SqlCommand();
+
+                Cmd.Connection = Con;
+
+                Cmd.CommandText = "Insert Into Product_Details (Product_Id, Brand_Name, Product_Name, Purchases_Price, Selling_Price, Stock, Distributor_Name, Date, GST, Description) Values (@PID, @B_Name, @P_Name, @PP, @SP, @Stock, @D_Name, @Date, @GST, @Descrip)";
+
+                Cmd.Parameters.Add("PID", SqlDbType.Int).Value = tb_Product_ID.Text;
+                Cmd.Parameters.Add("B_Name", SqlDbType.NVarChar).Value = cmb_Brand_Name.Text;
+                Cmd.Parameters.Add("P_Name", SqlDbType.NVarChar).Value = tb_Product_Name.Text;
+                Cmd.Parameters.Add("PP", SqlDbType.Money).Value = tb_Purchase_Price.Text;
+                Cmd.Parameters.Add("SP", SqlDbType.Money).Value = tb_Selling_Price.Text;
+                Cmd.Parameters.Add("Stock", SqlDbType.Int).Value = tb_Stock.Text;
+                Cmd.Parameters.Add("D_Name", SqlDbType.NVarChar).Value = cmb_Distributor_Name.Text;
+                Cmd.Parameters.Add("Date", SqlDbType.Date).Value = dtp_Date.Value.Date;
+                Cmd.Parameters.Add("GST", SqlDbType.Float).Value = tb_GST.Text;
+                Cmd.Parameters.Add("Descrip", SqlDbType.NVarChar).Value = tb_Description.Text;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Product Details Saved Successfully !!!", "Saving", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Clear_Control();
+            }
+             
+            else
+            {
+                MessageBox.Show("Incomplete Information !!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+             
+            Con_Close();
         }
 
         private void btn_Refresh_Click(object sender, EventArgs e)
