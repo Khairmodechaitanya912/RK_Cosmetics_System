40614d952469e671a669eb46296d1beeca633ac2
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-05-10 17:06:35 +0530

    Report

--- RK_Cosmetics_System/RK_Cosmetics_System/Report/Customer_Report.Designer.cs
+++ RK_Cosmetics_System/RK_Cosmetics_System/Report/Customer_Report.Designer.cs
@@ -36,9 +36,9 @@
             this.crystalReportViewer1.ActiveViewIndex = -1;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(312, 374);
+            this.crystalReportViewer1.Location = new System.Drawing.Point(-1, 173);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(1014, 297);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1485, 625);
             this.crystalReportViewer1.TabIndex = 0;
             // 
             // Customer_Report
